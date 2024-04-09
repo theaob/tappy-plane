7d1b640d0426e524ab832dbf81cac1017444eb57
Onur Baykal <theaob@gmail.com>
2024-04-09 10:11:36 +0300

    add jump animation

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -4,6 +4,7 @@ const GRAVITY: float = 1500.0
 const POWER: float = -500.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 
 func _physics_process(delta):
 	fly(delta)
@@ -17,6 +18,7 @@ func fly(delta) -> void:
 	
 	if (Input.is_action_just_pressed("fly")):
 		velocity.y = POWER 
+		animation_player.play("power")
 		
 func die() -> void:
 	print("DEAD")
