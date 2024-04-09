ade574d5fd19b82b1830a196c5a3ac1bca79830e
Onur Baykal <theaob@gmail.com>
2024-04-09 09:57:44 +0300

    add physics logic to plane

---
+++ scenes/plane/plane.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const GRAVITY: float = 1500.0
+const POWER: float = -500.0
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	fly(delta)
+	move_and_slide()
+	
+	if is_on_floor() || is_on_ceiling():
+		die()
+
+func fly(delta) -> void:
+	velocity.y += GRAVITY * delta
+	
+	if (Input.is_action_just_pressed("fly")):
+		velocity.y = POWER 
+		
+func die() -> void:
+	print("DEAD")
+
+	set_physics_process(false)
+	animated_sprite_2d.stop()
