0b9a8010427722d7596420ffaae3c851451f82f7
Onur Baykal <theaob@gmail.com>
2024-04-09 12:23:49 +0300

    scene switch implemented

---
+++ scenes/main/main.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var highscore = $MarginContainer/Highscore
+
+func _ready():
+	highscore.text = str(ScoreManager.get_high_score())
+
+func _process(delta):
+	if Input.is_action_just_pressed("fly"):
+		GameManager.load_game_scene()
--- singletons/game_manager.gd
+++ singletons/game_manager.gd
@@ -2,3 +2,12 @@ extends Node
 
 const GROUP_PLAYER: String = "player"
 const SCROLL_SPEED: float = 120.0
+
+var main_scene: PackedScene = preload("res://scenes/main/main.tscn")
+var game_scene: PackedScene = preload("res://scenes/game/game.tscn")
+
+func load_game_scene():
+	get_tree().change_scene_to_packed(game_scene)
+
+func load_main_scene():
+	get_tree().change_scene_to_packed(main_scene)
